aa2d8b318952790143d3ebb8b0b49286d30de05a
Jon Witherspoon <119535757+theknoxtech@users.noreply.github.com>
2025-07-28 21:55:39 -0400

    Add FizzBuzz solution

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,27 +1,27 @@
 // FizzBuzz.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main()
 {	
-
-	for (int numbers = 0; numbers < 101; numbers++) 
-	{
-		cout << numbers << "\n";
+	for (int numbers = 1; numbers < 101; numbers++) 
+	{	
+		if (numbers % 3 == 0 && numbers % 5 != 0)
+		{
+			cout << "Fizz" <<  "\n";
+		}
+		else if (numbers % 5 == 0 && numbers % 15 != 0)
+		{
+			cout << "Buzz" <<  "\n";
+		}
+		else if (numbers % 15 == 0)
+		{
+			cout << "FizzBuzz" <<  "\n";
+		}
+		else
+		{
+			cout << numbers << "\n";
+		}
 	}
-
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
